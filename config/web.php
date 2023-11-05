fefe10dea9aff7d48c1dd002d704881abf4e59b6
dobrainya <silnbrains@gmail.com>
2023-11-05 11:11:21 -0500

    fix readme file

--- config/web.php
+++ config/web.php
@@ -92,7 +92,7 @@ if (YII_ENV_DEV) {
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
 //        'allowedIPs' => ['127.0.0.1', '::1'],
-        'allowedIPs' => ['*'],
+//        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
@@ -100,7 +100,7 @@ if (YII_ENV_DEV) {
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
 //        'allowedIPs' => ['127.0.0.1', '::1'],
-        'allowedIPs' => ['*'],
+//        'allowedIPs' => ['*'],
     ];
 }
 
